862b0e64424bf9438fde5d617e4ca3bbd2ab0aca
Ziyuan Zhang <zzhang949@wisc.edu>
2021-07-27 05:41:00 -0400

    update config

--- recall-service/recall/config.py
+++ recall-service/recall/config.py
@@ -2,6 +2,20 @@ import os
 
 config = {
     'dataset_path': '/home/ziyuan/PycharmProjects/gars/dataset',
+    'deepwalk': {
+        'sample_length': 10,
+        'sample_count': 100 * 1000
+    },
+    'item2vec': {
+        'vector_size': 5,
+        'max_iter': 5,
+        'window_size': 10
+    },
+    'redis': {
+        'host': '127.0.0.1',
+        'port': 6379,
+        'db': 0
+    },
     'most_rating': {
         'shuffle_sample': True
     },
