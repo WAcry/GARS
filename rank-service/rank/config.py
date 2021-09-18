01def57f7c95aa024085eaa0bdb64d8629937647
Ziyuan Zhang <zzhang949@wisc.edu>
2021-09-18 00:48:51 -0400

    update rank service config

--- rank-service/rank/config.py
+++ rank-service/rank/config.py
@@ -1,5 +1,6 @@
 import os
 
 config = {
-    'recall_endpoint': 'http://localhost:5001'
+    'recall_endpoint': 'http://localhost:5001',
+    'model_path': '/home/ziyuan/PycharmProjects/gars/rank-service/mlp_model'
 }
